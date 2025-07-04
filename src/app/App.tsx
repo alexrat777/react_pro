663155511d113bf711f29cfef4cbb7d8415d44dd
lysenko.ai <aleksandr.lysenko@dom.ru>
2025-07-04 19:57:31 +0400

    10 Архитектура.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import React, {Suspense, useContext, useMemo, useState} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './style/index.scss';
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {MainPageAsync} from "pages/MainPage/ui/MainPage.async";
+import {AboutPageAsync} from "pages/AboutPage/ui/AboutPage.async";
+import {classNames} from "shared/lib/helpers/classNames/classNames";
+import { useTheme } from './providers/ThemeProvider';
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 // 2 шаг для router<Routes> <Route path={'/'} element={<MainPage />}/> </Routes>
 // 3 шаг для router Link to={'/'}>Главная</Link>
 
@@ -19,8 +21,8 @@ const {toggleTheme,theme} = useTheme()
             <button onClick={toggleTheme}>Переключить тему</button>
             <Suspense fallback={<div>Загрузка...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />}/>
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />}/>
                 </Routes>
             </Suspense>
         </div>
