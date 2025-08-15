2f7becf1747ca0037446986f829c4bf20e44fc48
alexrat777 <rat.lex@gmail.com>
2025-08-15 19:12:18 +0400

    30 Redux-toolkit. Entity. Тесты на всех уровнях убрал счетчики со страниц

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Counter } from 'entities/Counter';
 
 const AboutPage = () => {
     const { t } = useTranslation('about');
     return (
         <div>
             {t('О сайте')}
-            <Counter />
-
         </div>
     );
 };
