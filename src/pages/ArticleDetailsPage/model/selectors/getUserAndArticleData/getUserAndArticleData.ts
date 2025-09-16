d324861d551d03bc7fa7ee8908e20fda67561e78
alexrat777 <rat.lex@gmail.com>
2025-09-16 16:21:58 +0400

    53 Апгрейд сайдбара. Селекторы  соединение 2 селекторов в 1 через реселект в addCommentForArticle.ts

---
+++ src/pages/ArticleDetailsPage/model/selectors/getUserAndArticleData/getUserAndArticleData.ts
@@ -0,0 +1,8 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { getUserAuthData } from 'entity/User';
+import { getArticleDetailsData } from 'entity/Article/model/selectors/selectors/articleDetails';
+
+export const getUserAndArticleData = createSelector(
+    [getUserAuthData, getArticleDetailsData],
+    (userData, article) => ({ userData, article }),
+);
