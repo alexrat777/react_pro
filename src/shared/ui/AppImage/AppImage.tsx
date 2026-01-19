c948ccc9018e6f3444052c0b6f13097fc4cd53e2
alexrat777 <rat.lex@gmail.com>
2026-01-19 19:22:45 +0400

    106 Работа с изображенями. AppImage, Preload, lazy

---
+++ src/shared/ui/AppImage/AppImage.tsx
@@ -0,0 +1,46 @@
+import { useTranslation } from 'react-i18next';
+import {
+    ImgHTMLAttributes, memo, ReactElement, useLayoutEffect, useState,
+} from 'react';
+
+interface AppImageProps extends ImgHTMLAttributes<HTMLImageElement>{
+    className?: string;
+    fallback?:ReactElement;
+    errorFallback?:ReactElement;
+}
+
+export const AppImage = memo((props: AppImageProps) => {
+    const {
+        className,
+        alt = 'image',
+        src,
+        fallback,
+        errorFallback,
+        ...otherProps
+    } = props;
+    const { t } = useTranslation();
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasError, setHasError] = useState(false);
+    // useLayoutEffect для вызова до того как компонент вмонтировался в дом
+    useLayoutEffect(() => {
+        const img = new Image();
+        img.src = src ?? '';
+        img.onload = () => {
+            setIsLoading(false);
+        };
+        img.onerror = (ev) => {
+            setHasError(true);
+            setIsLoading(false);
+            console.log(ev);
+        };
+    }, [src]);
+    if (isLoading && fallback) {
+        return fallback;
+    }
+    if (hasError && errorFallback) {
+        return errorFallback;
+    }
+    return (
+        <img className={className} alt={alt} src={src} {...otherProps} />
+    );
+});
--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -1,24 +1,38 @@
 import { CSSProperties, useMemo } from 'react';
 import { classNames } from '@/shared/lib/helpers/classNames/classNames';
 import cls from './Avatar.module.scss';
+import { AppImage } from '../AppImage';
+import UserIcon from '../../assets/icons/user-filled.svg';
+import { Icon } from '../Icon';
+import { Skeleton } from '../Skeleton';
 
 interface AvatarProps {
     className?: string;
     src?: string;
     size?: number;
     alt?: string;
+    fallbackInverted?: boolean;
 }
 
 export const Avatar = (props:AvatarProps) => {
     const {
-        className, src, size, alt,
+        className,
+        src,
+        size = 100,
+        alt,
+        fallbackInverted,
     } = props;
     const styles = useMemo<CSSProperties>(() => ({
-        width: size || '100',
-        height: size || '100',
+        width: size,
+        height: size,
     }), [size]);
+
+    const fallback = <Skeleton width={size} height={size} borderRadius="50%" />;
+    const errorFallback = <Icon inverted={fallbackInverted} Svg={UserIcon} width={size} height={size} />;
     return (
-        <img
+        <AppImage
+            fallback={fallback}
+            errorFallback={errorFallback}
             style={styles}
             src={src}
             className={classNames(cls.Avatar, {}, [className])}
