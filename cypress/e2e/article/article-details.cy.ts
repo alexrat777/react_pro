2247655c5bded67dabbf77ffe4fe27c57f308ff7
alexrat777 <rat.lex@gmail.com>
2026-02-17 14:57:29 +0400

    115 Моки. Стабы. Фикстуры. Скип тестов. Интерцепторы на запросы и фикстуры

--- cypress/e2e/article/article-details.cy.ts
+++ cypress/e2e/article/article-details.cy.ts
@@ -33,4 +33,11 @@ describe('Пользователь заходит на страницу стат
         cy.setRate(5,'feedback');
         cy.get('[data-selected=true').should('have.length',5);
     });
+    it('И ставит оценку(тест на стабах)', () => {
+        cy.intercept('GET','**/articles/*',{ fixture:'article-details.json' });
+        cy.getByTestId("ArticleDetails.Info");
+        cy.getByTestId("RatingCard").scrollIntoView();
+        cy.setRate(5,'feedback');
+        cy.get('[data-selected=true').should('have.length',5);
+    });
 });
