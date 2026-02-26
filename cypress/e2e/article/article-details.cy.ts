08e6497ef38df65e78a2a7570eb8fc30444e18b0
alexrat777 <rat.lex@gmail.com>
2026-02-26 17:12:06 +0400

    116 Изолированные тесты на компоненты с Cypress +eslint

--- cypress/e2e/article/article-details.cy.ts
+++ cypress/e2e/article/article-details.cy.ts
@@ -1,43 +1,40 @@
-import {Article} from "@/entities/Article";
-
-let currentArticleID ='';
+let currentArticleID = '';
 describe('Пользователь заходит на страницу статьи', () => {
     beforeEach(() => {
-        cy.login()
+        cy.login();
 
         cy.createArticle().then((article) => {
             currentArticleID = article.id;
-            cy.visit('articles/'+article.id);
+            cy.visit(`articles/${article.id}`);
             cy.log(JSON.stringify(article));
-
         });
     });
     afterEach(() => {
         cy.removeArticle(currentArticleID);
     });
     it('И видит содержимое статьи', () => {
-        cy.getByTestId("ArticleDetails.Info").should('exist');
+        cy.getByTestId('ArticleDetails.Info').should('exist');
     });
     it('И видит рекомендации', () => {
-        cy.getByTestId("ArticleRecommendationsList").should('exist');
+        cy.getByTestId('ArticleRecommendationsList').should('exist');
     });
     it('И оставляет комментарии', () => {
-        cy.getByTestId("ArticleDetails.Info").should('exist');
-        cy.getByTestId("AddCommentForm").scrollIntoView();
-        cy.addComment('text comment')
-        cy.getByTestId('CommentCard.Content').should('have.length',1);
+        cy.getByTestId('ArticleDetails.Info').should('exist');
+        cy.getByTestId('AddCommentForm').scrollIntoView();
+        cy.addComment('text comment');
+        cy.getByTestId('CommentCard.Content').should('have.length', 1);
     });
     it('И ставит оценку', () => {
-        cy.getByTestId("ArticleDetails.Info");
-        cy.getByTestId("RatingCard").scrollIntoView();
-        cy.setRate(5,'feedback');
-        cy.get('[data-selected=true').should('have.length',5);
+        cy.getByTestId('ArticleDetails.Info');
+        cy.getByTestId('RatingCard').scrollIntoView();
+        cy.setRate(5, 'feedback');
+        cy.get('[data-selected=true').should('have.length', 5);
     });
     it('И ставит оценку(тест на стабах)', () => {
-        cy.intercept('GET','**/articles/*',{ fixture:'article-details.json' });
-        cy.getByTestId("ArticleDetails.Info");
-        cy.getByTestId("RatingCard").scrollIntoView();
-        cy.setRate(5,'feedback');
-        cy.get('[data-selected=true').should('have.length',5);
+        cy.intercept('GET', '**/articles/*', { fixture: 'article-details.json' });
+        cy.getByTestId('ArticleDetails.Info');
+        cy.getByTestId('RatingCard').scrollIntoView();
+        cy.setRate(5, 'feedback');
+        cy.get('[data-selected=true').should('have.length', 5);
     });
 });
