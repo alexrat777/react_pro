56e7e6c5bde174d063ada60d770fe135c5b2d6be
alexrat777 <rat.lex@gmail.com>
2026-06-03 16:54:44 +0400

    144 Виджет с доп инфой о статье. i18n плюральные формы

--- src/shared/ui/redesigned/Card/Card.tsx
+++ src/shared/ui/redesigned/Card/Card.tsx
@@ -11,6 +11,7 @@ interface CardProps extends HTMLAttributes<HTMLDivElement> {
     children: React.ReactNode;
     variant?: CardVariant;
     max?: boolean;
+    fullHeight?: boolean;
     padding?: CardPadding;
     border?: CardBorder;
 }
@@ -28,17 +29,17 @@ export const Card = memo((props: CardProps) => {
         max,
         padding = '8',
         border = 'normal',
+        fullHeight,
         ...otherProps
     } = props;
     const paddingsClass = mapPaddingToClass[padding];
     return (
         <div
-            className={classNames(cls.Card, { [cls.max]: max }, [
-                className,
-                cls[variant],
-                cls[paddingsClass],
-                cls[border],
-            ])}
+            className={classNames(
+                cls.Card,
+                { [cls.max]: max, [cls.fullHeight]: fullHeight },
+                [className, cls[variant], cls[paddingsClass], cls[border]],
+            )}
             {...otherProps}
         >
             {children}
