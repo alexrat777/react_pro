5ea93935d0e65477358f1075d8f07e565a0ebdf0
alexrat777 <rat.lex@gmail.com>
2026-06-04 15:01:09 +0400

    145 Редизайн ArticleDetails

--- src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
+++ src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -1,9 +1,11 @@
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { classNames } from '@/shared/lib/helpers/classNames/classNames';
-import { Text, TextAlign } from '@/shared/ui/deprecated/Text';
+import { Text as TextDeprecated, TextAlign } from '@/shared/ui/deprecated/Text';
+import { Text } from '@/shared/ui/redesigned/Text';
 import cls from './ArticleImageBlockComponent.module.scss';
 import { ArticleImageBlock } from '../../model/types/article';
+import { ToggleFeatures } from '@/shared/lib/features';
 
 interface ArticleImageBlockComponentProps {
     className?: string;
@@ -28,7 +30,16 @@ export const ArticleImageBlockComponent = memo(
                     />
                 </div>
                 {block.title && (
-                    <Text text={block.title} align={TextAlign.CENTER} />
+                    <ToggleFeatures
+                        feature="isAppRedesigned"
+                        on={<Text text={block.title} align="center" />}
+                        off={
+                            <TextDeprecated
+                                text={block.title}
+                                align={TextAlign.CENTER}
+                            />
+                        }
+                    />
                 )}
             </div>
         );
